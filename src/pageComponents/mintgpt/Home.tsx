2de9497b94f6b54ecff19d6a92360dfad1296ec9
David Quintero <davidquintero@extropy.lan>
2024-02-04 14:17:28 -0500

    Layout component

--- src/pageComponents/mintgpt/Home.tsx
+++ src/pageComponents/mintgpt/Home.tsx
@@ -1,10 +1,12 @@
 import ThreeCity from '../../components/3D/3DCity';
 import Header from '../../components/header/Header';
+import Layout from './Layout';
 
 export default function Home() {
   return (
     <ThreeCity>
       <Header />
+      <Layout>Hello</Layout>
     </ThreeCity>
   );
 }
---
+++ src/pageComponents/mintgpt/Layout.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex h-[120px] justify-center pt-8">
+      <div className="container fixed z-10 h-[72px] px-8">
+        <div
+          className={clsx(
+            'flex flex-1 flex-grow items-center justify-between',
+            'rounded-[50px] border border-stone-300 bg-white bg-opacity-10 p-4 backdrop-blur-2xl',
+          )}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
