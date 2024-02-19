55a830ef437cb237d22e7ad38af4b32b2efcf486
David Quintero <davidquintero@extropy.lan>
2024-02-18 19:21:47 -0500

    home layout

--- src/pageComponents/mintgpt/Home.tsx
+++ src/pageComponents/mintgpt/Home.tsx
@@ -2,14 +2,63 @@ import ThreeCity from '../../components/3D/3DCity';
 import Header from '../../components/header/Header';
 import Layout from './Layout';
 
+type TextHeaderProps = {
+  children: React.ReactNode;
+};
+
+export function TextHeader({ children }: TextHeaderProps) {
+  return <h1 className="font-bebas text-3xl">{children}</h1>;
+}
+
+type TitleProps = {
+  text: string;
+};
+
+export function Title({ text }: TitleProps) {
+  return (
+    <span className="h-8 w-full">
+      <TextHeader>{text}</TextHeader>
+    </span>
+  );
+}
+
+type ContentWrapperProps = {
+  children: React.ReactNode;
+};
+
+function ContentWrapper({ children }: ContentWrapperProps) {
+  return <div className="flex h-5/6 w-full">{children}</div>;
+}
+
+function DallePrompt() {
+  return (
+    <div className="flex w-1/2 flex-col p-4">
+      <form>
+        <label htmlFor="prompt">
+          <div id="image-rendering">Image Preview</div>
+        </label>
+        <span className="">
+          <textarea
+            id="prompt"
+            name="prompt"
+            rows={3}
+            className="w-full rounded-lg border border-2 border-solid border-white bg-transparent backdrop-blur-2xl focus:border-pink-500 focus:outline-none"
+          />
+        </span>
+      </form>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <ThreeCity>
       <Header />
       <Layout>
-        <h1 className="font-bebas text-3xl">
-          Seamlessly prompt and mint your NFT collections — It&apos;s time to truly own your art.
-        </h1>
+        <Title text="Prompt and mint an NFT collection — own your art." />
+        <ContentWrapper>
+          <DallePrompt />
+        </ContentWrapper>
       </Layout>
     </ThreeCity>
   );
--- src/pageComponents/mintgpt/Layout.tsx
+++ src/pageComponents/mintgpt/Layout.tsx
@@ -6,12 +6,12 @@ type LayoutProps = {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex h-[120px] justify-center pt-8">
-      <div className="z-9 container fixed h-[72px] px-8">
+    <div className="flex h-[500px] justify-center pt-8">
+      <div className="z-9 container fixed h-[500px] px-8">
         <div
           className={clsx(
-            'flex flex-1 flex-grow items-center justify-between',
-            'rounded-[50px] border border-stone-300 bg-white bg-opacity-10 p-4 backdrop-blur-2xl',
+            'items-top flex flex-wrap',
+            'h-[700px] rounded-[10px] border border-stone-300 bg-white bg-opacity-10 p-4 backdrop-blur-2xl',
           )}
         >
           {children}
